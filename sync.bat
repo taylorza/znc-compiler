47814387b33bc0ddfd9ef32adc2992e2c440369a
taylorza <chris_taylor_za@hotmail.com>
2025-12-28 22:28:11 -0500

    Libraries from Iáń Júkéś

--- sync.bat
+++ sync.bat
@@ -13,6 +13,15 @@
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/sprite.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/string.znc /zdev/
 
-rem%1/../../emulator/hdfmonkey get %1/../../sdcard/tbblue-dev.sd /zdev/zncdemo.bas tests/zncdemo.bas
-rem %1/../../emulator/hdfmonkey get %1/../../sdcard/tbblue-dev.sd /zdev/zide.bas tests/zide.bas
+rem Libraries used with permission from Iáń Júkéś
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/datetime.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/intToMonth.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/joystick.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/mouse.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/strcontains.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/strtoint.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/version.znc /zdev/
+
+%1/../../emulator/hdfmonkey get %1/../../sdcard/tbblue-dev.sd /zdev/zncdemo.bas tests/zncdemo.bas
+%1/../../emulator/hdfmonkey get %1/../../sdcard/tbblue-dev.sd /zdev/zide.bas tests/zide.bas
 
