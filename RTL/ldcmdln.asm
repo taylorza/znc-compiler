9f95cce6b915e730ee046dc7ae1a04149c97d718
taylorza <chris_taylor_za@hotmail.com>
2026-03-30 17:07:47 -0400

    Handle quotes in command line

--- RTL/ldcmdln.asm
+++ RTL/ldcmdln.asm
@@ -1,20 +1,37 @@
  ld de,_args
- ld bc,79
+ ld b,79
+ xor a
+ ld (.q),a
 .l1
  ld a, (hl)
  or a
  jr z,.l2
+ cp """"
+ jr z,.l3
+ ld c,a
+ ld a,(.q)
+ dec a
+ ld a,c
+ jr z,.l4
  cp ":"
  jr z,.l2
  cp 13
  jr z,.l2
+.l4
  ld (de), a
  inc hl
  inc de
- dec bc
- ld a, b
- or c
+ dec b
  jr nz,.l1
 .l2
  xor a
  ld (de), a
+ ret
+.l3
+  ld b,a
+  ld a,(.q)
+  xor 1
+  ld (.q), a
+  ld a,b
+  jr .l4
+.q db 0
