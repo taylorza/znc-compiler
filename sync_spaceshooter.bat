ecfa32c17102df3d3060fe924ef233f4d7c1fdf6
Chris Taylor <taylorza@users.noreply.github.com>
2026-06-13 19:22:10 -0400

    Spaceshooter improvements

--- sync_spaceshooter.bat
+++ sync_spaceshooter.bat
@@ -1,1 +1,4 @@
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spaceshooter.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spaceshooter.spr /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spaceshooter.til /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spaceshooter.pal /zdev/
