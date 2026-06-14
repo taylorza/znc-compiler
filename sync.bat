a92c5c6141999411f6e2b03a22063b860c9fdf5d
Chris Taylor <taylorza@users.noreply.github.com>
2026-06-14 11:30:50 -0400

    Add additional spaceshooter resource files to sync script

--- sync.bat
+++ sync.bat
@@ -13,6 +13,9 @@
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/rubik.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/libcompile.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spaceshooter.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spaceshooter.spr /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spaceshooter.til /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spaceshooter.pal /zdev/
 
 
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/ay.znc /zdev/
