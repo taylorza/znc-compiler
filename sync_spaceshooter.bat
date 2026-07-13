89169b8508738abfca7c646ad355b80e40f38e53
Chris Taylor <taylorza@users.noreply.github.com>
2026-07-12 20:31:07 -0400

    Release prep

--- sync_spaceshooter.bat
+++ sync_spaceshooter.bat
@@ -4,6 +4,7 @@
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spaceshooter.pal /zdev/examples/
 
 
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/grep.znc /zdev/examples/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/regextst.znc /zdev/examples/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/sudoku.znc /zdev/examples/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/PUZZLE01.txt /zdev/examples/
@@ -11,3 +12,9 @@
 
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/crt.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/crtedit.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/cmdline.znc /zdev/
+
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/printf.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/sprintf.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/format.znc /zdev/
+
