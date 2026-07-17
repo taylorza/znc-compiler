d7dbecb88cfab01b12f63953998cf53a1747b6a6
Chris Taylor <taylorza@users.noreply.github.com>
2026-07-17 16:23:53 -0400

    Update ZNC Compiler version to v0.6d-beta and enhance printf/sprintf functionality

--- sync.bat
+++ sync.bat
@@ -41,9 +41,13 @@
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/regex.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/sprite.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/string.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/vsscanf.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/sscanf.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/vsprintf.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/printf.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/sprintf.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/printf_tiny.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/sprintf_tiny.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/kbd.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/mmu.znc /zdev/
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/cmdline.znc /zdev/
--- sync_spaceshooter.bat
+++ sync_spaceshooter.bat
@@ -4,9 +4,11 @@ rem %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spacesho
 rem %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/spaceshooter.pal /zdev/examples/
 
 %1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/a.znc /zdev/examples/
-%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/configtst.znc /zdev/examples/
-%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/config.txt /zdev/examples/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/fmttst.znc /zdev/examples/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd tests/sudoku.znc /zdev/examples/
 
-%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/vsprintf.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/sprintf.znc /zdev/
+%1/../../emulator/hdfmonkey put %1/../../sdcard/tbblue-dev.sd znclib/printf.znc /zdev/
+
 
 
